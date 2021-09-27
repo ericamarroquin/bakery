9c4dd4ebda054400967e3ef65b83df0768f91097
EricaMarroquin <ericamarroquin03@gmail.com>
2021-09-26 19:56:53 -0700

    add user interface to ask quantity of baked goods wanted and return total cost

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Bakery.Models;
 
 namespace Bakery
@@ -7,7 +8,21 @@ namespace Bakery
   {
     public static void Main()
     {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Bread is $5 per loaf, where every third loaf is free.");
+      Console.WriteLine("Pasties are $2 for 1 or $5 for 3.");
       
+      Console.WriteLine("How many loaves of bread would you like?");
+      string userBreadQuantity = Console.ReadLine();
+
+      Console.WriteLine("How many pastries would you like?");
+      string userPastryQuantity = Console.ReadLine();
+
+      Bread loavesOfBread = new Bread(int.Parse(userBreadQuantity));
+      Pastry numberOfPastries = new Pastry(int.Parse(userPastryQuantity));
+      int totalCost = loavesOfBread.Cost() + numberOfPastries.Cost();
+
+      Console.WriteLine("Thank you! Your total cost today is $" + totalCost + ".");
     }
   }
 }
