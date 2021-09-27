24d9e2bac99b8ff2895c882348b3315b60707a5f
EricaMarroquin <ericamarroquin03@gmail.com>
2021-09-26 19:33:34 -0700

    add Program file for UI

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -9,15 +9,11 @@ namespace Bakery.Models
       Quantity = quantity;
     }
 
-    public int Cost() // 6
+    public int Cost()
     {
-      int quantityDivisibleByTwo = Quantity % 3; // 0 
-      int quantityDivisibleByThree = (Quantity - quantityDivisibleByTwo) / 3; // (6 - 0) / 3 = 6 / 3 = 2
-      return (quantityDivisibleByTwo * 2) + (quantityDivisibleByThree * 5); // (0*2)+(2*5) = 0+10 = 10
+      int quantityDivisibleByTwo = Quantity % 3;
+      int quantityDivisibleByThree = (Quantity - quantityDivisibleByTwo) / 3;
+      return (quantityDivisibleByTwo * 2) + (quantityDivisibleByThree * 5);
     }
   }
 }
-
-// Pastry: Buy 1 for $2 or 3 for $5. 
-// Four pastries costs $7, five pastries costs $9, 
-// and six pastries costs $10.
